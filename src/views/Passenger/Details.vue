7f9c51770d1afa4943e359e46f16b36542d9a72e
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-25 12:02:28 +0700

    5.4 Update code, Add slogan

--- src/views/Passenger/Details.vue
+++ src/views/Passenger/Details.vue
@@ -1,14 +1,16 @@
 <template>
   <hr />
   <div v-if="passenger">
-    <h1>{{ passenger.name }}</h1>
-    <h3>{{ passenger.airline.country }}</h3>
+    <h1>Name: {{ passenger.name }}</h1>
+    <h3> {{ passenger.airline.country }}</h3>
     <h5>{{ passenger.airline.head_quaters }}</h5>
     <p>{{ passenger.airline.website }}</p>
+    <h3>{{ passenger.airline.slogan }}</h3>
     <span v-for="airline in passenger.airline" :key="airline.id">
       <h3>{{ airline.country }}</h3>
       <h5>{{ airline.head_quaters }}</h5>
       <p>{{ airline.website }}</p>
+      <h3>{{ airline.slogan }}</h3>
     </span>
   </div>
 </template>
