2dee43cd60234ab9eeb27d18b57d05d01e3e32ee
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-25 12:16:33 +0700

    5.6 Provide the Resource not found
    when browsing for the id which does not exist

--- src/views/Passenger/NotFound.vue
+++ src/views/Passenger/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Oops!</h1>
-  <h3>The page you're looking fir is not here</h3>
+  <h3>The {{ resource }} you're looking fir is not here</h3>
   <router-link :to="{ name: 'PassengerList' }">Back to the home page</router-link>
 </template>
 
