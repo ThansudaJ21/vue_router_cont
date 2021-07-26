bad39b00b159eca2ad175ba0708572aecca614fb
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-27 02:23:39 +0700

    5.6 update passenger details

--- src/views/PassengerDetails.vue
+++ src/views/PassengerDetails.vue
@@ -1,17 +1,24 @@
 <template>
   <h1>Details</h1>
-<hr>
+  <hr />
   <div v-if="passenger">
-    <h1>{{ passenger.name }}</h1>
-    <h3>{{ passenger.airline.country }}</h3>
-    <h5>{{ passenger.airline.head_quaters }}</h5>
-    <p>{{ passenger.airline.website }}</p>
-    <h2>Slogan: {{ passenger.airline.slogan }}</h2>
-    <span v-for="airline in passenger.airline" :key="airline.id">
-      <h3>{{ airline.country }}</h3>
-      <h5>{{ airline.head_quaters }}</h5>
-      <p>{{ airline.website }}</p>
-    </span>
+    <h1>Name: {{ passenger.name }}</h1>
+    <div v-if="check">
+      <h2>Airline name: {{ passenger.airline.name }}</h2>
+      <h3>Slogan: {{ passenger.airline.slogan }}</h3>
+      <h4>Country: {{ passenger.airline.country }}</h4>
+      <p>Head quaters: {{ passenger.airline.head_quaters }}</p>
+      <p>Website: {{ passenger.airline.website }}</p>
+    </div>
+    <div v-if="check == false">
+      <span v-for="airline in passenger.airline" :key="airline.id">
+        <h2>Airline name: {{ airline.name }}</h2>
+        <h3>Slogan: {{ airline.slogan }}</h3>
+        <h4>Country: {{ airline.country }}</h4>
+        <p>Head quaters: {{ airline.head_quaters }}</p>
+        <p>Website: {{ airline.website }}</p>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -33,6 +40,16 @@ export default {
       .catch((error) => {
         console.log(error)
       })
+  },
+  computed: {
+    check: function () {
+      var check = this.id
+      if (check == '5f1c59d0fa523c3aa793c0ac') {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
