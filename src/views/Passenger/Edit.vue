2cf1d1ef89d3e4b14e0fb74c4127304a05196766
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-29 03:01:13 +0700

    5.1 Add Edit component and add button 'Edit Profile'

--- src/views/Passenger/Edit.vue
+++ src/views/Passenger/Edit.vue
@@ -1,10 +1,12 @@
 <template>
   <hr />
   <p>Edit the event here</p>
+  <button @click="register">Edit Profile</button>
 </template>
 
 <script>
 export default {
   props: ['passenger']
+  
 }
 </script>
